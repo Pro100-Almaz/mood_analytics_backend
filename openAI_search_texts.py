79a967ac84c6fb6cdd88c36207a1fba0e665fe6e
Almaz <q19.almaz@gmail.com>
2025-02-22 21:15:18 +0500

    fix

--- openAI_search_texts.py
+++ openAI_search_texts.py
@@ -1,7 +1,11 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
 
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+load_dotenv()
+
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 import json
 
 
